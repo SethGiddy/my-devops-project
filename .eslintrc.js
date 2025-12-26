8a3e537dec5bf4e1f9e1a6dcef40da7fd653e077
SethGiddy <79220387+SethGiddy@users.noreply.github.com>
2025-12-26 23:08:05 +0000

    Update .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+{
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2025": true
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-unused-vars": ["warn"],
+    "no-console": "off",
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"]
+  }
+}
